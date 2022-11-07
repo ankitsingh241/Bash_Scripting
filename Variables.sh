ae3fbd365d77ab9fff6370191403bcd651db8977
Ankit <iamnakitsingh24@gmail.com>
2022-11-07 16:07:04 +0530

    Variables examples

---
+++ Variables.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+# Set up the value of a variable: variableName=value
+
+    #myVar=10
+
+    #echo $myVar #Dollar sign must be placed before variable otherwise it will not run. 
+
+# command line argument
+
+    #echo $0 #$0 reflects the name of the fie.
+
+    #echo $USER
+
+    #cp $1 $2 #it will copy the first file that will be mentioned in bash command                 e.g. : ./Variable.sh file. txt Sample/ :-- txt file will be copied                      in Sample directory.
+
+
+ var1=Hello
+ var2=World
+ echo $var1 $var2
+
+
+
+
+
+
+
